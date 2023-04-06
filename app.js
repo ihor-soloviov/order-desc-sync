201d0e4eaa9ad2f2bc19d587813f4b2b8d7ae6cc
Ihor Soloviov <igor.musson.55@gmail.com>
2023-04-06 13:46:22 +0300

    simplified app.js file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { scheduleJob } from "node-schedule";
+import { allAddressesLine } from "./utils/allAddressesLine.js";
 
 const apiKey = "UtFSGa4gkqoCgkyXowthHhCLq9mioQNQLBu7nvgzAskcG7Eoot";
 const storeId = 52114;
@@ -14,17 +15,11 @@ const headers = {
 const synchronizeNewOrders = async () => {
   try {
     const response = await axios.get("https://app.orderdesk.me/api/v2/orders/new", { headers })
-    response.data.orders.forEach(order => {
-      const {shipping, id} = order;
-      const address = shipping.address1 || shipping.address2 || shipping.address3 || shipping.address4;
-      const shippingAddres = `${shipping.country}, ${shipping.state}, ${shipping.city}, ${address}`
-            console.log(`New order: ID ${id}, shipping address: ${shippingAddres}`);
-    })
+    allAddressesLine(response)
     
   } catch(error) {
     console.error(`Error fetching new orders from OrderDesk API: ${error.message}`);
   }
 };
 
-// Schedule script to run hourly
-scheduleJob('47 12 * * *', synchronizeNewOrders);
+scheduleJob('43 13 * * *', synchronizeNewOrders);
